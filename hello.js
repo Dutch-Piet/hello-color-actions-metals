c6add058e497b4f5e2586ad65fa9443d1233c13e
Dutch-Piet.github.io <141590672+Dutch-Piet@users.noreply.github.com>
2024-01-15 20:57:07 -0500

    updated greeting from Dutch-Piet

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hello World from the the Great White North! This is Dutch Piet from Toronto Canada on 2024-01-15";
 console.log(hello());
 exports.hello = hello;
