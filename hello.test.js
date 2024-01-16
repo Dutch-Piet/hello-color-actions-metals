5e6f4bdcd9e1388a23479dd8ea3facca78229c02
Dutch-Piet.github.io <141590672+Dutch-Piet@users.noreply.github.com>
2024-01-15 21:11:03 -0500

    updated what to expect in the Dutch-Piet Greeting

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hello World from the Office Hours in the terminal!");
+    expect(hello.hello()).toEqual("Hello World from the the Great White North! This is Dutch Piet from Toronto Canada on 2024-01-15");
   });
 });
