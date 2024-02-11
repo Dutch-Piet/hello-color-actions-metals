171064445532091391a3c6b7087f89da3ad4e253
Dutch-Piet.github.io <141590672+Dutch-Piet@users.noreply.github.com>
2024-02-11 15:42:25 -0500

    1st commit 2024-02-11 3:42pm

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,20 @@
 const hello = require("./hello");
-describe("My hello", () => {
-  test("works", () => {
-    expect(hello.hello()).toEqual("Hello World from the the Great White North! This is Dutch Piet from Toronto Canada on 2024-01-15");
+describe('hello function', () => {
+  it('returns the correct greeting message', () => {
+    const expected = `
+    <p style="font-size: 16px;"
+    >Hello World from the Great White North! <br> 
+    This is Dutch Piet from Toronto, Canada. <br> 
+    <br>
+    README: <br> 
+    1) This page fetches the commodity spot price from www.DailyMetalPrice.com. for the following metals used in a battery <br>
+      - Copper (Cu)<br> 
+      - Nickel (Ni) <br>
+      - Zinc (Zn) <br>
+    2) The user can then transcribe those values into the calculator table and add the qty of each metal used in the battery. <br>
+    3) When the 'calculate' button is pressed, the table updates to compute extended costs for each metal and the grand total cost for the product.
+    </p>
+    `;
+    expect(hello()).toBe(expected.trim());
   });
 });
